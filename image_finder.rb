3e1c8597085a73cb00d5de9403466ffd261fa99b
Adam Lloyd <lloyda2@rpi.edu>
2010-01-17 20:01:21 -0500

    Add a really simple (for now) image finder class.

---
+++ image_finder.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+require 'exifr'
+require 'find'
+
+class ImageFinder
+  include Enumerable
+
+  def initialize(directory)
+    @images = []
+
+    Find.find(directory) do |file|
+      if file.downcase =~ /\.jpg$/
+        @images << file
+        puts "Found #{file}"
+      end
+    end
+  end
+
+  def each
+    @images.each do |image|
+      yield image
+    end
+  end
+end
