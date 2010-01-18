9953b9c7e94e4783b2660afdc23fbe92242b4293
Adam Lloyd <lloyda2@rpi.edu>
2010-01-18 16:28:36 -0500

    Test generalized code in mimi.rb.

--- mimi.rb
+++ mimi.rb
@@ -1,16 +1,28 @@
 #!/usr/bin/env ruby
-require 'Qt4'
-require_relative 'importer.rb'
+# require 'Qt4'
+# require_relative 'importer.rb'
+require_relative 'image_finder.rb'
+require_relative 'image_importer.rb'
 
-app = Qt::Application.new(ARGV)
+# app = Qt::Application.new(ARGV)
 
-base_directory = ARGV[0]
-if base_directory.nil?
-  STDERR.puts "Please specify a directory."
+from_directory = ARGV[0]
+if from_directory.nil?
+  STDERR.puts "Please specify a directory from which to import."
   exit 1
 end
+to_directory = ARGV[1]
+if to_directory.nil?
+  STDERR.puts "Please specify a destination directory."
+  exit 2
+end
 
-importer = Importer.new(base_directory)
-importer.show()
+# importer = Importer.new(base_directory)
+# importer.show()
 
-app.exec()
+# app.exec()
+
+finder = ImageFinder.new(from_directory) {|file| puts "Found #{file}"}
+
+importer = ImageImporter.new(finder, to_directory, true)
+importer.go(true)
