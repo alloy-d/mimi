d09f1d723dd29531fd37d893420774721df34389
Adam Lloyd <lloyda2@rpi.edu>
2010-01-19 16:04:57 -0500

    Use new generalized framework in graphical importer.

--- qt/importer.rb
+++ qt/importer.rb
@@ -1,13 +1,21 @@
 #!/usr/bin/env ruby
 require 'Qt4'
+require_relative '../image_finder.rb'
 require_relative 'descriptive_progress_bar.rb'
 require_relative 'image_list.rb'
 
 class Importer < Qt::Widget
-  slots 'allowImport()', 'setNumImages(int)', 'setNumVideos(int)'
+  slots 'beginFinding()', 'allowImport()', 'doImport()'
+  signals 'newImageFile(QString)', 'doneFinding()', 'importReady()'
 
-  def initialize(directory, parent=nil)
+  attr_accessor :num_images
+
+  def initialize(from_directory, to_directory, parent=nil)
     super(parent)
+
+    @from_directory = from_directory
+    @to_directory = to_directory
+    @num_images = 0
 
     @progress_bar = DescriptiveProgressBar.new()
     @progress_bar.setText(tr('Loading...') + " (%m)")
@@ -16,16 +24,12 @@ class Importer < Qt::Widget
     @progress_bar.setEnabled(false)
 
     @button = Qt::PushButton.new(tr("Go!"))
-    #connect(button, SIGNAL('clicked()'), self, SLOT('allowImport()'))
+    connect(@button, SIGNAL('clicked()'), self, SLOT('doImport()'))
     @button.hide()
 
-    @image_list = ImageList.new(directory)
-    connect(@image_list, SIGNAL('doneLoading()'),
-            self, SLOT('allowImport()'))
-    connect(@image_list, SIGNAL('newNumImages(int)'),
-            self, SLOT('setNumImages(int)'))
-    connect(@image_list, SIGNAL('imageAdded()'),
-            self, SLOT('repaint()'))
+    @image_list = ImageList.new
+    connect(self, SIGNAL('newImageFile(QString)'),
+            @image_list, SLOT('addImage(QString)'))
 
     main_layout = Qt::VBoxLayout.new()
     main_layout.addWidget(@image_list)
@@ -38,6 +42,20 @@ class Importer < Qt::Widget
     setLayout(main_layout)
 
     setWindowTitle('mimi')
+
+    connect(self, SIGNAL('doneFinding()'), self, SLOT('allowImport()'))
+    @start_timer = Qt::Timer::singleShot(60, self, SLOT('beginFinding()'))
+#    @start_timer.start()
+  end
+
+  def beginFinding
+    @Finder = ImageFinder.new(@from_directory) do |file|
+      STDERR.puts file
+      self.emit newImageFile(file)
+      self.num_images = self.num_images + 1
+      STDERR.puts self.num_images
+    end
+    emit doneFinding()
   end
 
   def allowImport
@@ -46,11 +64,12 @@ class Importer < Qt::Widget
     @button.show()
   end
 
-  def setNumImages(new_num)
-    @progress_bar.setMaximum(new_num)
-    @progress_bar.update()
+  def setNumVideos(new_num)
   end
 
-  def setNumVideos(new_num)
+  def num_images=(new_num)
+    @num_images = new_num
+    @progress_bar.setMaximum(new_num)
+#    @progress_bar.update()
   end
 end
